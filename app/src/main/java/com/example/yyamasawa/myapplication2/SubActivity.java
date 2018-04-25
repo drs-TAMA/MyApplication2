c7c1011993d4c7b17a401d11e83d3aa0dcdd8af8
YAMASAWA-PC\y.yamasawa <yuhto0227@gmai.com>
2018-04-25 17:02:37 +0900

    Initial commit

---
+++ app/src/main/java/com/example/yyamasawa/myapplication2/SubActivity.java
@@ -0,0 +1,20 @@
+package com.example.yyamasawa.myapplication2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        Intent intent = this.getIntent();
+        String text = intent.getStringExtra("sendText");
+        TextView textView = (TextView)this.findViewById(R.id.textViewSub);
+        textView.setText(text);
+    }
+}
